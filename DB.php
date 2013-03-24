e67cfb35c08bd01eaab2cb988ec8cec9324f2140
bigonese <eric.bigoness@firelit.com>
2013-03-23 21:27:43 -0400

    WIP big updates to DB class - moved to PDO

--- DB.php
+++ DB.php
@@ -9,10 +9,13 @@ class DB {
 	
 	// Global connection & state variables 
 	public static $conn = false;
-	public static $database = false;
+	
+	public static $validTableName = '/^[a-zA-Z_][a-zA-Z0-9_]*$/'; // Regex for acceptable table names
+	public static $validColName = '/^[a-zA-Z_][a-zA-Z0-9_]*$/'; // Regex for acceptable column names
 	
 	// Object variables
-	private $res = false, $sql;
+	private $res = false;
+	private $sql;
 	
 	public function __construct() {
 		// Connect to the database
@@ -24,71 +27,157 @@ class DB {
 		$dsn = 'mysql:dbname='. FIRELIT_DB_NAME .';host='. FIRELIT_DB_IP;
 		
 		try {
-			$conn = new PDO($dsn, FIRELIT_DB_USER, FIRELIT_DB_PASS);
+			self::$conn = new PDO($dsn, FIRELIT_DB_USER, FIRELIT_DB_PASS);
 		} catch (PDOException $e) {
 			throw new Exception('Unable to connect to database.');
 		}
-
-  	self::$conn = $conn;
-  	return $conn;
+		
+  	return self::$conn;
   	
 	}
 	
-	public function query($sql) {
+	public function query($sql, $data) {
 		// Execute the SQL query
-		return $this->res = self::$conn->query($sql);
+		// Pass $sql statement and $data to bind with the $keys matching placeholders (see PDO prepare docs for details)
+		// Example: query( "SELECT * FROM `table` WHERE `col`=:theval", array(':theval' => 'Hello!') )
+		
+		$binder = array();
+		foreach ($array as $col => &$val) // By reference for db driver purposes
+			$binder[(preg_match('/^:/', $col) ? '' : ':').$col] = $val; 
+		
+		$this->sql = self::$conn->prepare($sql);
+		
+		return $this->res = $this->sql->execute($binder);
+		
 	}
 	
 	public function insert($table, $array) {
 		// Preform an insert on the table
-		$sql = "INSERT INTO `". $table ."` ". self::toSQL('INSERT', $array);
+		// Enter an associative array for $array with column names as keys
 		
-		// Returns the number of affected rows
-		return self::$conn->exec($sql);
+		if (!preg_match(self::$validTableName, $table)) 
+			throw new Exception('Invalid database table name specified.');
+		
+		$this->sql = self::$conn->prepare("INSERT INTO `". $table ."` ". self::prepInsert($array));
+		
+		$binder = array();
+		foreach ($array as $col => &$val) // By reference for db driver purposes
+			$binder[':'.$col] = $val;
+		
+		return $this->res = $this->sql->execute($binder);
+		
 	}
 	
 	public function replace($table, $array) {
 		// Preform an replace on the table
-		$sql = "REPLACE INTO `". $table ."` ". self::toSQL('INSERT', $array);
+		// Enter an associative array for $array with column names as keys
 		
-		// Returns the number of affected rows
-		return self::$conn->exec($sql);
+		if (!preg_match(self::$validTableName, $table)) 
+			throw new Exception('Invalid database table name specified.');
+		
+		$this->sql = self::$conn->prepare("REPLACE INTO `". $table ."` ". self::prepInsert($array));
+		
+		$binder = array();
+		foreach ($array as $col => &$val) // By reference for db driver purposes
+			$binder[':'.$col] = $val;
+		
+		return $this->res = $this->sql->execute($binder);
+		
 	}
 	
-	public function select($table, $where = 1, $limit = false, $range = false) {
+	public function select($table, $select = array(), $where = 1, $whereData = array(), $limit = false, $range = false) {
 		// Preform an select on the table
-		$sql = "SELECT * FROM `". $table ."` WHERE ". $where;
+		// Enter column names in $select array or leave blank (or false) for all (ie, *)
+		// Enter where clause into $where or leave as 1 to select all
+		// Enter $whereData as associative array with data to be escaped and inserted into where clause (see PDO prepare docs for details)
+		// $limit and $range should be integers used as SQL LIMIT
+		
+		if (!preg_match(self::$validTableName, $table)) 
+			throw new Exception('Invalid database table name specified.');
+		
+		if (is_array($select) && sizeof($select)) {
+			
+			$selectSql = '';
+			
+			foreach ($select as $col) {
+				
+				if (!preg_match(self::$validColName, $col)) 
+					throw new Exception('Invalid database column name specified.');
+				
+				$selectSql .= ', `'. $select .'`';
+				
+			}
+			
+			$selectSql = substr($selectSql, 2);
+			
+		} else $selectSql = '*';
+		
+		$sql = "SELECT ". $selectSql ." FROM `". $table ."` WHERE ". $where;
+		
 		if ($limit !== false) {
 			$sql .= " LIMIT ". intval($limit);
 			if ($range !== false) $sql .= ", ". intval($range);
 		}
 		
-		// Returns the result set for the query
-		return $this->res = self::$conn->query($sql);
+		$this->sql = self::$conn->prepare($sql);
+		
+		$binder = array();
+		foreach ($whereData as $col => &$val) // By reference for db driver purposes
+			$binder[(preg_match('/^:/', $col) ? '' : ':').$col] = $val; 
+		
+		return $this->res = $this->sql->execute($binder);
+		
 	}
 	
-	public function update($table, $array, $where, $limit = false, $range = false) {
+	public function update($table, $array, $where, $whereData = array(), $limit = false, $range = false) {
 		// Preform an update on the table
-		$sql = "UPDATE `". $table ."` SET ". self::toSQL('UPDATE', $array) ." WHERE ". $where;
-		if ($limit !== false) {
-			$sql .= " LIMIT ". intval($limit);
-			if ($range !== false) $sql .= ", ". intval($range);
-		}
+		// Enter an associative array for $array with column names as keys
+		// Enter $whereData as associative array with data to be escaped and inserted into where clause (see PDO prepare docs for details)
+		// $where placeholders (referenced in $whereData) cannot be column names used in the $array
 		
-		// Returns the number of affected rows
-		return self::$conn->exec($sql, $array);
+		if (!preg_match(self::$validTableName, $table)) 
+			throw new Exception('Invalid database table name specified.');
+		
+		if (sizeof(array_intersect_key($array, $whereData)))
+			throw new Exception('Conflicting parameter names for binding.');
+		
+		$this->sql = self::$conn->prepare("UPDATE `". $table ."` SET ". self::prepUpdate($array) ." WHERE ". $where);
+		
+		$binder = array();
+		
+		foreach ($array as $col => &$val) // By reference for db driver purposes
+			$binder[':'.$col] = $val;
+		
+		foreach ($whereData as $col => &$val) // By reference for db driver purposes
+			$binder[(preg_match('/^:/', $col) ? '' : ':').$col] = $val; 
+			
+		return $this->res = $this->sql->execute($binder);
+		
 	}
 	
-	public function delete($table, $where, $limit = false, $range = false) {
+	public function delete($table, $where, $whereData = array(), $limit = false, $range = false) {
 		// Preform a delete on the table
+		// Enter $whereData as associative array with data to be escaped and inserted into where clause (see PDO prepare docs for details)
+		
+		if (!preg_match(self::$validTableName, $table)) 
+			throw new Exception('Invalid database table name specified.');
+		
 		$sql = "DELETE FROM `". $table ."` WHERE ". $where;
+		
 		if ($limit !== false) {
 			$sql .= " LIMIT ". intval($limit);
 			if ($range !== false) $sql .= ", ". intval($range);
 		}
 		
-		// Returns the number of affected rows
-		return self::$conn->exec($sql);
+		$this->sql = self::$conn->prepare($sql);
+		
+		$binder = array();
+		
+		foreach ($whereData as $col => &$val) // By reference for db driver purposes
+			$binder[(preg_match('/^:/', $col) ? '' : ':').$col] = $val; 
+		
+		return $this->res = $this->sql->execute($binder);
+		
 	}
 	
 	public function getRes() {
@@ -97,133 +186,83 @@ class DB {
 	
 	public function getRow() {
 		if (!$this->res) return false;
-		return mysql_fetch_assoc($this->res);
+		return $this->sql->fetch(PDO::FETCH_ASSOC);
+	}
+	
+	public function getAll() {
+		if (!$this->res) return false;
+		return $this->sql->fetchAll(PDO::FETCH_ASSOC);
 	}
 	
 	public function getNewId() {
-		return mysql_insert_id(self::$conn);
+		return self::$conn->lastInsertId();
 	}
 	
 	public function getAffected() {
-		return mysql_affected_rows(self::$conn);
+		return self::$conn->rowCount();
 	}
 	
 	public function getNumRows() {
-		return mysql_num_rows($this->res);
+		return $this->sql->rowCount();
 	}
 	
 	public function getError() {
-		return mysql_error(self::$conn);
+		$e = $this->sql->errorInfo();
+		return $e[2]; // Driver specific error message.
+	}
+	
+	public function getErrorCode() {
+		$e = $this->sql->errorInfo();
+		return $e[1]; // Driver specific error code.
 	}
 	
 	public function success($file = false, $line = false) {
-		if (!$this->res && $file) new LogIt(3, 'MySql error ('. $this->getError() .', '. $this->sql .')', $file, $line);
+		if (!$this->res && $file) new LogIt(3, 'MySql error ('. $this->getErrorCode() .', '. $this->getError() .', '. $this->sql .')', $file, $line);
 		return $this->res;
 	}
 	
-	public static function asl($txt) {
-		if (!self::$conn) self::connect();
-		if (is_array($txt)) throw new Exception('Must pass a string to add slashes.');
-		return mysql_real_escape_string($txt);	
+	public static function prepInsert($array) {
+		// Prepare an INSERT SQL statement ready for a data bind
+		
+		if (!is_array($array)) 
+			throw new Exception('Method expects an array.');
+		
+		$sql1 = '';
+		$sql2 = '';
+		
+		foreach ($array as $col => $val) {
+			
+			if (!preg_match(self::$validColName, $col)) 
+				throw new Exception('Invalid database column name specified.');
+			
+			$sql1 .= ', `'. $col .'`';
+			$sql2 .= ", :". $col;
+			
+		}
+		
+		return '( '. substr($sql1, 2) . ' ) VALUES ( '. substr($sql2, 2) .' )';
+			
 	}
 	
-	public static function toSQL($verb, $assocArray) {
-		// $assocArray should be an array of 'raw' items (not yet sanitized for database)
-		// If the type is ambiguous, the item should be array(type, value)
-		// Acceptable types: boolean, integer, float, double, string, sql, serialize
+	public static function prepUpdate($array) {
+		// Prepare an UPDATE SQL statement ready for a data bind
 		
-		if ($verb == 'INSERT') {
+		if (!is_array($array)) 
+			throw new Exception('Method expects an array.');
+	
+		$sql = '';
+		
+		foreach ($array as $col => $val) {
 			
-			$sql1 = '';
-			$sql2 = '';
+			if (!preg_match(self::$validColName, $col)) 
+				throw new Exception('Invalid database column name specified.');
 			
-			foreach ($assocArray as $key => $value) {
-				
-				switch (gettype($value)) {
-					case 'boolean':
-					case 'integer':
-					case 'double':
-					case 'string':
-						$type = gettype($value);
-						break;
-					case 'array': $type = lower($value[0]); $value = $value[1]; break;
-					case 'NULL': $type = 'string'; $value = ''; break;
-					default: throw new Exception('Invalid type for '. __FUNCTION__ .'(): '. gettype($value));
-				}
-				
-				$sql1 .= ', `'. self::asl($key) .'`';
-				
-				switch ($type) {
-					case 'boolean':
-						$sql2 .= ", ". ($value ? 'TRUE' : 'FALSE');
-						break;
-					case 'integer':
-					case 'double':
-					case 'float':
-						$sql2 .= ", ". $value;
-						break;
-					case 'string':
-						$sql2 .= ", '". self::asl($value) ."'";
-						break;
-					case 'sql':
-						$sql2 .= ", ". $value;
-						break;
-					case 'serialize':
-						$sql2 .= ", '". self::asl(serialize($value)) ."'";
-						break;
-					default: throw new Exception('Invalid type for '. __FUNCTION__ .'(): '. gettype($value));
-				}
-				
-			}
+			$sql .= ', `'. $col .'` = :'. $col;
 			
-			return '('. substr($sql1, 2) . ') VALUES ('. substr($sql2, 2) .')';
-			
-		} elseif ($verb == 'UPDATE') {
-			
-			$sql = '';
-			
-			foreach ($assocArray as $key => $value) {
-				
-				switch (gettype($value)) {
-					case 'boolean':
-					case 'integer':
-					case 'double':
-					case 'string':
-						$type = gettype($value);
-						break;
-					case 'array': $type = lower($value[0]); $value = $value[1]; break;
-					case 'NULL': $type = 'string'; $value = ''; break;
-					default: throw new Exception('Invalid type for '. __FUNCTION__ .'(): '. gettype($value));
-				}
-				
-				$sql .= ', `'. $key .'`=';
-				
-				switch ($type) {
-					case 'boolean':
-						$sql .= ($value ? 'TRUE' : 'FALSE');
-						break;
-					case 'integer':
-					case 'double':
-					case 'float':
-						$sql .= $value;
-						break;
-					case 'string':
-						$sql .= "'". self::asl($value) ."'";
-						break;
-					case 'sql':
-						$sql .= $value;
-						break;
-					case 'serialize':
-						$sql .= "'". self::asl(serialize($value)) ."'";
-						break;
-					default: throw new Exception('Invalid type for '. __FUNCTION__ .'(): '. gettype($value));
-				}
-				
-			}
-			
-			return substr($sql, 2);
-			
-		} else throw new Exception("Invalid verb for toSQL();");
+		}
+		
+		return substr($sql, 2);
+					
 	}
 	
 }
