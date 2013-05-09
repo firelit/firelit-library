b927fc183ca0786abd25234e0997d3d8fb2170e1
Eric Bigoness <eric.bigoness@firelit.com>
2013-05-09 11:59:39 -0400

    WIP Session unittest work

--- tests/SessionTest.php
+++ tests/SessionTest.php
@@ -13,22 +13,42 @@ class SessionTest extends PHPUnit_Framework_TestCase {
 			mt_rand(1, 100000000)
 		);
 		
-		$this->store = Firelit\SessionStore::init('PHP');
-		$this->session = new Firelit\Session($store);
+		$this->store = Firelit\SessionStore::init('Mem');
+		$this->session = new Firelit\Session($this->store);
 		
 	}
-
+		
 	public function testSetGet() {
+	
 		$varName = 'test'. mt_rand(0, 1000);
 		$this->session->$varName = $this->testVal;
+		
 		$this->session->flushCache();
-		$this->assertEqual($this->session->$varName, $this->testVal);
+		
+		$this->assertEquals($this->session->$varName, $this->testVal);
+		
+	}
+		
+	public function testUnset() {
+	
+		$varName = 'test'. mt_rand(0, 1000);
+		$this->session->$varName = $this->testVal;
+		
+		unset($this->session->$varName);
+		
+		$this->assertNull($this->session->$varName);
+		
 	}
 	
+	
 	public function testDestroy() {
+	
 		$varName = 'test'. mt_rand(0, 1000);
 		$this->session->$varName = $this->testVal;
+		
 		$this->session->destroy();
+		
 		$this->assertNull($this->session->$varName);
+		
 	}
 }
