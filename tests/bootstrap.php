c02177ced92bf1c11cd568a4c00978386c41fe12
Eric Bigoness <eric.bigoness@firelit.com>
2013-05-09 14:01:49 -0400

    Rearranged unittest directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ function bootstrapAutoloader($class) {
 	$parts = explode('\\', $class);
 	if ($parts[0] != 'Firelit') 
 		throw new Exception('Invalid class for autoloading: '. $parts[0] .', '. $class);
-	require_once('../'. $parts[1] .'.php');
+	require_once($parts[1] .'.php');
 }
 
 spl_autoload_register('bootstrapAutoloader');
