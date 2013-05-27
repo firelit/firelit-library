5b5a00bc19d81b1b3510c20ba3edc271e44d6e78
Eric Bigoness <eric.bigoness@firelit.com>
2013-05-27 15:55:10 -0400

    WIP adding new QueryTest

---
+++ tests/QueryTest.php
@@ -0,0 +1,45 @@
+<?PHP
+
+class QueryTest extends PHPUnit_Framework_TestCase {
+	
+	protected $q, $res;
+	
+	protected function setUp() {
+		
+		Firelit\Query::config(array(
+			'type' => 'other',
+			'dsn' => 'sqlite::memory'
+		));
+		
+		$this->q = new Firelit\Query();
+		$this->res = $this->q->query("CREATE TABLE IF NOT EXISTS `Tester` (`id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, `name` TINYTEXT, `date` DATETIME, `state` BOOL)");
+		
+	}
+	
+	public function testConstructor() {
+		
+		// If setup() worked, then this will pass!
+		$this->assertTrue($this->res);
+		
+	}
+	
+	public function testInsert() {
+		
+		$this->q->insert('Tester', array(
+			'name' => 'John',
+			'date' => array('SQL', "DATETIME('now')"),
+			'state' => false
+		));
+		
+		$this->assertTrue( $q->success() );
+		
+		
+	}
+	
+	protected function tearDown() {
+		
+		$this->q->query("DROP TABLE `Tester`");
+		
+	}
+	
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,11 +4,9 @@ function bootstrapAutoloader($class) {
 	$parts = explode('\\', $class);
 	if ($parts[0] != 'Firelit') 
 		throw new Exception('Invalid class for autoloading: '. $parts[0] .', '. $class);
-	require_once($parts[1] .'.php');
+		
+	if (file_exists($parts[1] .'.php'))
+		require_once($parts[1] .'.php');
 }
 
 spl_autoload_register('bootstrapAutoloader');
-
-Firelit\Query::config(array(
-	
-));
