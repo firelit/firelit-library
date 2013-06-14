e11635774efc86874eb9d85639a8017e2b16e96d
bigonese <eric.bigoness@firelit.com>
2013-06-14 12:50:03 -0400

    Added load balancing support to ServerRequest for getting remote IP and proxy data

--- lib/Firelit/ServerRequest.php
+++ lib/Firelit/ServerRequest.php
@@ -5,11 +5,16 @@ namespace Firelit;
 class ServerRequest {
 	
 	// All properties accessible via magic getter method
-	private $host, $path, $method, $secure, $referer, $cli, $headers;
+	private $ip, $proxies, $host, $path, $method, $secure, $referer, $cli, $headers;
 	private $post, $get, $cookie;
+	
+	public static $loadBalanced = false;
 
+	// $filter should be a filtering function, if supplied, which filters a string value by reference
 	public function __construct($filter = false) { 
 		
+		$this->ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : false;
+		$this->proxies = array();
 		$this->method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : false;
 		$this->path = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : false;
 		$this->secure = isset($_SERVER['HTTPS']) ? ($_SERVER['HTTPS'] == 'on') : false;
@@ -18,6 +23,28 @@ class ServerRequest {
 		$this->cli = (php_sapi_name() == 'cli');
 		
 		$this->headers = apache_request_headers();
+		
+		if (self::$loadBalanced) {
+			
+			if (isset($this->headers['X-Forwarded-For'])) {
+				$ips = $this->headers['X-Forwarded-For'];
+				$ips = explode(', ', $ips);
+				$this->ip = array_shift($ips);
+				
+				$this->proxies = $ips;
+			}
+			
+			if (isset($this->headers['X-Forwarded-Proto'])) {
+				$this->secure = ($this->headers['X-Forwarded-Proto'] == 'HTTPS');
+			}
+			
+		} elseif (isset($this->headers['X-Forwarded-For'])) {
+			
+			$ips = $this->headers['X-Forwarded-For'];
+			$ips = explode(', ', $ips);
+			$this->proxies = $ips;
+			
+		}
 		
 		$this->post = $_POST;
 		$this->get = $_GET;
